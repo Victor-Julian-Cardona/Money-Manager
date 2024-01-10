3bc2028e474d5e3f7df6f978b84297fb07a32a04
Victor-Julian-Cardona <victor.cardona5@gmail.com>
2024-01-10 15:01:47 -0400

    Added login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import { AuthContext } from './context/auth.context'
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import { useContext } from "react";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { AuthContext } from "./context/auth.context";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 function App() {
-  const { getToken } = useContext(AuthContext)
+  const { getToken } = useContext(AuthContext);
 
-  const isLoggedIn = () => {
-    return getToken() ? <Outlet /> :
-  }
+  const IsLoggedIn = () => {
+    return getToken() ? <Outlet /> : <Navigate to="/login" />;
+  };
+
+  const IsLoggedOut = () => {
+    return !getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
 
   return (
     <>
-    <Navbar />
+      <Navbar />
 
-    <Routes>
-      
-      <Route path ='/' element={<Home />} />
-      <Route path ='/login' element={<Login />} />
-      <Route path ='/signup' element={<Signup />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-    </Routes>
+        <Route element={<IsLoggedOut />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+
+        <Route element={<IsLoggedIn />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
